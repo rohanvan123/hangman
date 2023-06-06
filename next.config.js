3fcb404b22a9381344a61a3f7429e80f0b163fa8
rohanvan123 <rv4711@zebra.com>
2023-06-06 09:42:26 -0500

    Test for deployment again

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = nextConfig
 const isGithubActions = process.env.GITHUB_ACTIONS || false
 
 let assetPrefix = ''
-let basePath = '/'
+let basePath = ''
 
 if (isGithubActions) {
   const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
